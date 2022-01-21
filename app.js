b48b8118a186539f72c114138f5450633a652d49
Akshat Gupta <thisisakshat@protonmail.com>
2022-01-21 19:51:51 +0530

    Implemented CORS

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -25,8 +26,18 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1) GLOBAL MIDDLEWARES
+//////////////////////////// Implement CORS(all routes) : Access-Control-Allow-Origin *
+app.use(cors());
+// If to allow specific domains:
+// app.use(cors({
+//   origin:'https://www.natours.com'
+// }))
 
-// Seerving static files
+// Responding to options request for CORS (preflight phase)
+app.options('*', cors());
+// app.options('/api/v1/tours/:id', cors());
+
+// Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Set security HTTP headers
