ffb259d8a127c0b9c232a7aca2faafbc72034c2a
Akshat Gupta <thisisakshat@protonmail.com>
2022-01-21 20:49:07 +0530

    Improved stripe implementation

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const viewRouter = require('./routes/viewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
+const bookingController = require('./controllers/bookingController');
 
 // Starting application
 const app = express();
@@ -50,6 +51,15 @@ const limiter = rateLimit({
   message: 'Too many requests received. Please try again in an hour!'
 });
 app.use('/api', limiter);
+
+/*This MUST be before body parser because stripe needs
+ the checkout session data that it put on req.body in RAW format, 
+ NOT JSON!!*/
+app.post(
+  '/webhook-checkout',
+  express.raw({ type: 'application/json' }),
+  bookingController.webhookCheckout
+);
 
 // Body parser: reading data from body to req.body
 app.use(express.json({ limit: '10kb' }));
